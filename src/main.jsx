681db9e8255021cb9ea6e93d37bd474ce0d6ec90
jacobhogsten <hogsten1996@gmail.com>
2023-11-13 14:07:41 -0500

    commit 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
-import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
-import store from "./store";
-import "./app.css";
-import Nav from "./components/Nav";
+// import {Provider} from "react-redux";
+// import {BrowserRouter} from "react-router-dom";
+// import store from "./store";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Provider store={store}>
-                <Nav/>
+        {/* <BrowserRouter> */}
+            {/* <Provider store={store}> */}
                 <App/>
-            </Provider>
-        </BrowserRouter>
+            {/* </Provider> */}
+        {/* </BrowserRouter> */}
     </React.StrictMode>
 );
